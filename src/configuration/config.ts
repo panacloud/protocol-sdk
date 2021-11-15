9f8abda17c670951b5940ea5a0da73bef1b24db3
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-15 18:45:28 +0500

    Added network config with dummy address for now

---
+++ src/configuration/config.ts
@@ -0,0 +1,21 @@
+export interface ContractNetworkConfig {
+    
+    /** panaFactoryAddress - Address of the PanaFactory contract deployed on a specific network */
+    panaFactoryAddress: string
+}
+
+export interface ContractNetworksConfig {
+    /** id - Network id */
+    [id: string]: ContractNetworkConfig
+}
+
+export const defaultContractNetworks : ContractNetworksConfig = {
+    // mainnet
+    1: {
+        panaFactoryAddress:"" 
+    },
+    // rinkeby
+    4: {
+        panaFactoryAddress:"0x8D29bE29923b68abfDD21e541b9374737B49cdAD" 
+    }
+}
