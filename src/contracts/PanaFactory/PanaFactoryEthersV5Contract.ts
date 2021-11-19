e18765fb0ff4145902b0f57c5b8da5ce6520132f
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-19 23:37:55 +0500

    Added Ethers implementation of PanaFactoryContract which wraps actual PanaFactory contract

---
+++ src/contracts/PanaFactory/PanaFactoryEthersV5Contract.ts
@@ -0,0 +1,25 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import PanaFactoryContract from "./PanaFactoryContract";
+import { PanaFactory, PanaFactoryInterface } from '../../../typechain/PanaFactory';
+
+
+
+class PanaFactoryEthersV5Contract implements PanaFactoryContract {
+
+    constructor(public contract:PanaFactory) {
+
+    }
+
+    generateData(_name:string, _age:BigNumber ) {
+        this.contract.generateData(_name, _age);
+    }
+    
+    getName():Promise<string> {
+        return this.contract.getName();
+    }
+    
+    getAge():Promise<BigNumber> {
+        return this.contract.getAge();
+    }
+}
+export default PanaFactoryEthersV5Contract;
