9bf3f721b676fe3541e4b19c87078a49322736b7
zeeshanhanif <zeeshanhanif@gmail.com>
2022-01-31 02:08:08 +0500

    Update PanacloudPlatfromContract to include more functionality that help user fetch details

--- src/contracts/PanacloudPlatform/PanacloudPlatformContract.ts
+++ src/contracts/PanacloudPlatform/PanacloudPlatformContract.ts
@@ -1,12 +1,12 @@
 import { BigNumber } from '@ethersproject/bignumber';
-import { APIDAOConfig, APITokenConfig, BaseTransactionResult, TransactionOptions, Invoice } from '../../utils/types';
+import { APIDAOConfig, APITokenConfig, BaseTransactionResult, TransactionOptions, Invoice, APIDevDetails } from '../../utils/types';
 
 
 interface PanacloudPlatformContract {
   getAddress(): string;
   payInvoice(apiDev:string, apiDao:string, invoice:Invoice): Promise<BaseTransactionResult>;
-  //getDevEarnings(): string;
-  //getAPIInvoices(): string;
+  getDevEarnings(apiDevAddress: string): Promise<APIDevDetails>;
+  getAPIInvoices(apiDevAddress: string, apiTokenAddress: string): Promise<Invoice[]>;
 }
 
 export default PanacloudPlatformContract
--- src/contracts/PanacloudPlatform/PanacloudPlatformEthersV5Contract.ts
+++ src/contracts/PanacloudPlatform/PanacloudPlatformEthersV5Contract.ts
@@ -1,7 +1,7 @@
 import { BigNumber } from '@ethersproject/bignumber';
 
 import { PanacloudPlatform  } from '../../../typechain/src/ethers-v5/PanacloudPlatform';
-import { APIDAOConfig, APITokenConfig, BaseTransactionResult, EthersTransactionResult, Invoice, TransactionOptions } from '../../utils/types';
+import { APIDAOConfig, APIDevDetails, APITokenConfig, BaseTransactionResult, EthersTransactionResult, Invoice, TransactionOptions } from '../../utils/types';
 import { ContractTransaction } from '@ethersproject/contracts';
 import PanacloudPlatformContract from './PanacloudPlatformContract';
 
@@ -23,12 +23,28 @@ class PanacloudPlatformEthersV5Contract implements PanacloudPlatformContract {
     }
 
     getAddress(): string {
-        return this.contract.address;
+      return this.contract.address;
     }
 
     async payInvoice(apiDev: string, apiDao: string, invoice: Invoice, options?:TransactionOptions): Promise<EthersTransactionResult> {
-        const txtResponse = await this.contract.payInvoice(apiDev, apiDao,invoice);
-        return toTxResult(txtResponse, options);
+      const txtResponse = await this.contract.payInvoice(apiDev, apiDao,invoice);
+      return toTxResult(txtResponse, options);
+    }
+
+    async getDevEarnings(apiDevAddress: string): Promise<APIDevDetails> {
+      const devResponseObj = await this.contract.getDevEarnings(apiDevAddress);
+      const apiDevDetails:APIDevDetails = {
+        apiDev: apiDevAddress,
+        totalEarned: devResponseObj[0],
+        totalClaimable: devResponseObj[1],
+        totalClaimed: devResponseObj[2],
+        userDAODetails: devResponseObj[3]
+      }
+      return apiDevDetails;
+    }
+
+    async getAPIInvoices(apiDevAddress: string, apiTokenAddress: string): Promise<Invoice[]> {
+      return this.contract.getAPIInvoices(apiDevAddress,apiTokenAddress);;
     }
 }
 export default PanacloudPlatformEthersV5Contract;
