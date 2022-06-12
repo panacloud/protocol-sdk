620780d0ae611af39326970a37532bbffff80cde
zeeshanhanif <zeeshanhanif@gmail.com>
2022-06-13 03:19:24 +0500

    Added invement pool and manager into config for address list

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -2,7 +2,9 @@ export interface ContractNetworkConfig {
     
     /** panaFactoryAddress - Address of the PanaFactory contract deployed on a specific network */
     panaFactoryAddress: string,
-    panacloudPlatform: string
+    panacloudPlatform: string,
+    investmentPools: string,
+    investmentPoolsManager: string   
 }
 
 export interface ContractNetworksConfig {
@@ -19,6 +21,8 @@ export const defaultContractNetworks : ContractNetworksConfig = {
     // rinkeby
     4: {
         panaFactoryAddress:"0x8C252d76FAE65509E388f88c72DB489F02c41094",
-        panacloudPlatform:"0xa8234cCcec0AeD396C5aD9721c5d2dB5c8E035e8"
+        panacloudPlatform:"0xa8234cCcec0AeD396C5aD9721c5d2dB5c8E035e8",
+        investmentPools:"0xa8234cCcec0AeD396C5aD9721c5d2dB5c8E035e8",
+        investmentPoolsManager:"0xa8234cCcec0AeD396C5aD9721c5d2dB5c8E035e8"
     }
 }
