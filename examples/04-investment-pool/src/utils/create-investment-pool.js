63ceb4f5a12c8e5b406e624d7fd9577b751f8ff5
zeeshanhanif <zeeshanhanif@gmail.com>
2022-09-02 23:52:25 +0500

    Updated create investment pool script

--- examples/04-investment-pool/src/utils/create-investment-pool.js
+++ examples/04-investment-pool/src/utils/create-investment-pool.js
@@ -11,12 +11,15 @@ export const createInvestmentPool = async (signer)=>{
 
     const panacloudSDK = await PanacloudSDK.create({ethAdapter});
     console.log("PanacloudPlatform Address",panacloudSDK.getPanacloudPlatform().getAddress());
-
+    
+    let whitelistingStartDate = new Date("8/20/2022");
+    let whitelistingEndDate = new Date("9/10/2022");
     const investmentPoolsContract = panacloudSDK.getInvestmentPools();
     const transactionResult = await investmentPoolsContract.createInvestmentPool(
         "0xb11846818eda46eca2e0481a4a4afebb4cac18d5","0xa1182eBDc63a68a5355235132aF9AD7555C39c03",
         BigNumber.from((new Date()).getTime()),BigNumber.from(30).mul(24).mul(60).mul(60),BigNumber.from(100),
-        10000,BigNumber.from(7000), BigNumber.from(100));
+        10000,BigNumber.from(7000), BigNumber.from(100),
+        BigNumber.from(whitelistingStartDate.getTime()), BigNumber.from(whitelistingEndDate.getTime()));
 
     console.log("Transaction hash: ",transactionResult.hash);
     const transactionReceipt = await transactionResult.transactionResponse.wait();
