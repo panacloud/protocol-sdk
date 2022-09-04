8126b5edfba5cc07d78e2b29f3ddabd66c13a7f0
zeeshanhanif <zeeshanhanif@gmail.com>
2022-09-04 14:02:04 +0500

    Updated contract addresses in config

--- src/configuration/config.ts
+++ src/configuration/config.ts
@@ -20,9 +20,9 @@ export const defaultContractNetworks : ContractNetworksConfig = {
     },*/
     // rinkeby
     4: {
-        panaFactoryAddress:"0x8aD97bC24eAAf59Db2930835252959bb1b932e95",
-        panacloudPlatform:"0x75F94f1c60a2fBdD8AEF8F62B14267db2D541229",
-        investmentPools:"0xFCd122B2881424ef6bB187359B4c4ABDb81E7599",
+        panaFactoryAddress:"0xd2b18870440EB6ca0EdB95A1dc589Be3754cd793",
+        panacloudPlatform:"0xc3Cf63E29907E0C8D79B0FF42c402fA17ffF12CB",
+        investmentPools:"0x01183Cd242F0Ea65Fe6024c5801C6c43133eEfF0",
         investmentPoolsManager:"0xCD7F39E2e076Da03a30E5491ebFffA2aa2b73655"
     }
 }
